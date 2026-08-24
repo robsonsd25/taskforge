5422383cff5faaa9649c06480ddc89d9481a722f
Robson R Sousa <robsonsd25@gmail.com>
2026-08-24 11:00:26 -0300

    atualização task forge

--- TaskForge.java
+++ TaskForge.java
@@ -4,30 +4,50 @@ void main() {
     String nome = IO.readln("Nome da tarefa: ");
     String descricao = IO.readln("Descrição: ");
 
-    int prioridade = Integer.parseInt(
-            IO.readln("Prioridade (1 a 5): ")
-    );
+    int prioridade = 0;
+    do {
+        prioridade = Integer.parseInt(IO.readln("Prioridade (1 a 5): "));
+        if (prioridade < 1 || prioridade > 5) {
+            IO.println("Prioridade inválida.");
+        }
+    } while (prioridade < 1 || prioridade > 5);
 
-    while (prioridade < 1 || prioridade > 5) {
-        IO.println("ERRO: A prioridade deve ser um número entre 1 e 5.");
-
-        prioridade = Integer.parseInt(
-                IO.readln("Digite novamente a prioridade (1 a 5): ")
-        );
-    }
-
+    IO.println();
     String responsavel = IO.readln("Responsável: ");
-    double horasEstimadas = Double.parseDouble(
-            IO.readln("Horas estimadas: ")
-    );
+    double horasEstimadas = Double.parseDouble(IO.readln("Horas estimadas: "));
 
-    String status = "pendente";
+    int status;
+    do {
+        IO.println("Escolha uma opção: ");
+        IO.println("1 - Pendente");
+        IO.println("2 - Em Andamento");
+        IO.println("3 - Concluída");
+        IO.println("4 - Cancelada");
+        status = Integer.parseInt(IO.readln());
+        if (status < 1 || status > 4) {
+            IO.println("Status inválido.");
+        }
+    } while (status < 1 || status > 4);
 
     IO.println("");
     IO.println("------ TAREFA CRIADA ------");
     IO.println("Tarefa:     " + nome);
     IO.println("Descrição:  " + descricao);
-    IO.println("Prioridade: " + prioridade);
-    IO.println("Status:     " + status);
+    IO.print("Prioridade: ");
+    for (int aux = 0; aux < prioridade; aux++) {
+        IO.print("*");
+    }
+    IO.println();
+
+    String statusNome = "";
+    switch (status) {
+        case 1 -> statusNome = "Pendente";
+        case 2 -> statusNome = "Em Andamento";
+        case 3 -> statusNome = "Concluída";
+        case 4 -> statusNome = "Cancelada";
+        default -> statusNome = "Status Inválido";
+    }
+
+    IO.println("Status:    " + statusNome);
     IO.println("---------------------------");
 }
