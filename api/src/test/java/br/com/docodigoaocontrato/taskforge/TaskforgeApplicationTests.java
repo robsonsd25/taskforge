4aa624a4b140a2cf569762729618f81f93805924
Robson R Sousa <robsonsd25@gmail.com>
2026-09-09 22:42:59 -0300

    TarefaDTO

---
+++ api/src/test/java/br/com/docodigoaocontrato/taskforge/TaskforgeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.docodigoaocontrato.taskforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskforgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
